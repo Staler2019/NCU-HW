e381326b800db4c273d297ed6951c9dba6a8aacd
staler2019 <abcd121234@yahoo.com.tw>
2022-01-01 01:25:53 +0800

    22.1.1 Compiler FP end p.3

---
+++ Compiler/Final project/src/struct.h
@@ -0,0 +1,17 @@
+#ifndef _STRUCT_HPP_
+#define _STRUCT_HPP_
+
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+extern "C" {
+int yylex(void);
+}
+
+struct Type_UNION {
+}
+
+#define YYSTYPE Type_UNION
+
+#endif // !_STRUCT_HPP_
